5e2c6678c455d8d11e74e5485ce54f6f58ae8d90
Santringoding <96803898+santringodingid@users.noreply.github.com>
2024-06-03 14:48:04 +0700

    fix(guardian): set image value in db

--- app/Http/Controllers/RegisterManagement/GuardianController.php
+++ app/Http/Controllers/RegisterManagement/GuardianController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\RegisterManagement;
 
 use App\Exports\GuardianExport;
 use App\Http\Controllers\Controller;
+use App\Models\RegisterManagement\Guardian;
+use App\Models\RegisterManagement\Student;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
@@ -17,5 +19,27 @@ class GuardianController extends Controller
     public function export()
     {
         return (new GuardianExport())->download('data-wali-'.session()->get('hijri').'.xlsx');
+    }
+
+    public function setImage()
+    {
+        $guardians = Guardian::where('image', NULL)->get();
+        if($guardians->count() > 0){
+            foreach($guardians as $guardian){
+                $guardian->image = 'avatars/guardians/'.$guardian->id.'.jpg';
+                $guardian->save();
+            }
+        }
+
+        $students = Student::where('image_of_profile', NULL)->get();
+        if($students->count() > 0){
+            foreach($students as $student){
+                $student->image_of_profile = 'avatars/students/'.$student->id.'.jpg';
+                $student->image_of_signature = 'images/signatures/'.$student->id.'.png';
+                $student->save();
+            }
+        }
+
+        return redirect()->route('register-management.guardian');
     }
 }
